809a248a7cecc2a8d5146f927ad9b75c2ec6af1c
KishoreMenda <m.kishore0309@gmail.com>
2024-04-21 12:23:42 -0700

    forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react'
+
 const App = () => {
+  const [data,setData] = useState({
+    email : '',
+    password : ''
+  })
+  const changeHandler = (e) => {
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  const submithandler = (e) => {
+    e.preventDefault();
+    if(data.password.length<5) {
+      alert('password length should be greater than 5')
+    } else {
+      console.log(data);
+    }
+  }
   return (
     <div>
       <center>
-        <h3>this is a sample text</h3>
+        <form onSubmit={submithandler}>
+          <label> e-mail:</label>
+          <input type="email" name="email" onChange={changeHandler}></input><br></br>
+          <label> password:</label>
+          <input type="password" name ="password" onChange={changeHandler}></input><br></br>
+          <input type="submit" value="Login"></input>
+        </form>
       </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
